096ed009a3ebf37928793fda4ac22c869f4a28d3
magic3dcn <magic3d@163.com>
2014-09-23 16:45:27 +0800

    Update.

--- Src/AppModules/FaceRecognition.cpp
+++ Src/AppModules/FaceRecognition.cpp
@@ -31,22 +31,22 @@ namespace MagicApp
         std::ifstream fin(faceImgFiles);
         int dataSize;
         fin >> dataSize;
+        std::vector<int> faceIdList(dataSize);
         std::vector<std::string> imgFileNames(dataSize);
         std::vector<double> markList;
         int markCount = 0;
-        const int maxSize = 512;
-        char pLine[maxSize];
-        fin.getline(pLine, maxSize);
         for (int dataId = 0; dataId < dataSize; dataId++)
         {
-            fin.getline(pLine, maxSize);
-            std::string imgName(pLine);
+            int faceId;
+            std::string imgName;
+            std::string markFileName;
+            fin >> faceId >> imgName >> markFileName;
+
+            faceIdList.at(dataId) = faceId;
             imgName = imgPath + imgName;
-            imgFileNames.at(dataId) = imgName;
-            
-            std::string markFileName = imgName;
-            std::string::size_type pos = markFileName.rfind(".");
-            markFileName.replace(pos, 4, ".mark");
+            imgFileNames.at(dataId) = imgName;         
+
+            markFileName = imgPath + markFileName;
             std::ifstream markFin(markFileName);
             markFin >> markCount;
             if (dataId == 0)
@@ -72,6 +72,7 @@ namespace MagicApp
         std::vector<cv::Point2f> cvCurMarks(markCount);
         for (int iterIndex = 0; iterIndex < iterCount; iterIndex++)
         {
+            //DebugLog << "iterIndex: " << iterIndex << std::endl;
             if (iterIndex == 0)
             {
                 for (int markId = 0; markId < markCount; markId++)
@@ -94,7 +95,9 @@ namespace MagicApp
                     cvCurMarks.at(markId).x = markList.at(markBaseIndex + markId * 2);
                     cvCurMarks.at(markId).y = markList.at(markBaseIndex + markId * 2 + 1);
                 }
+                //DebugLog << " transMat, dataId: " << dataId << std::endl;
                 cv::Mat transMat = cv::estimateRigidTransform(cvCurMarks, cvMeanMarks, false);
+                //DebugLog << "     finished" << std::endl;
                 if (transMat.data == NULL)
                 {
                     DebugLog << "    transMat.data == NULL: " << dataId << " " << iterIndex << std::endl;
@@ -124,6 +127,8 @@ namespace MagicApp
         }
         
         DebugLog << "Align each face to mean face and extract it" << std::endl;
+        std::string fileListName = alignedFilePath + "/fileList.txt";
+        std::ofstream fileListOut(fileListName);
         //Align each face to mean face and extract it
         int validAlignedId = 0;
         for (int dataId = 0; dataId < dataSize; dataId++)
@@ -140,16 +145,18 @@ namespace MagicApp
                 DebugLog << "miss face: " << dataId << " : " << imgFileNames.at(dataId) << std::endl;
                 continue;
             }
+            fileListOut << faceIdList.at(dataId) << " t_" << validAlignedId << ".jpg" << " t_" << validAlignedId << ".land" << std::endl;
+
             cv::Mat img = cv::imread(imgFileNames.at(dataId));
             cv::Mat alignedImg(img.rows, img.cols, img.type());
             cv::warpAffine(img, alignedImg, transMat, alignedImg.size());
             img.release();
             std::stringstream ss;
-            ss << alignedFilePath << "_" << validAlignedId << ".jpg";
+            ss << alignedFilePath << "/t_" << validAlignedId << ".jpg";
             std::string alignedImgName;
             ss >> alignedImgName;
             cv::imwrite(alignedImgName, alignedImg);
-            alignedImg.release();
+            //alignedImg.release();
             
             MagicMath::HomoMatrix3 homoMat;
             homoMat.SetValue(0, 0, transMat.at<double>(0, 0));
@@ -159,23 +166,31 @@ namespace MagicApp
             homoMat.SetValue(1, 1, transMat.at<double>(1, 1));
             homoMat.SetValue(1, 2, transMat.at<double>(1, 2));
             ss.clear();
-            ss << alignedFilePath << "_" << validAlignedId << ".mark";
+            ss << alignedFilePath << "/t_" << validAlignedId << ".land";
             std::string alignedMarkName;
             ss >> alignedMarkName;
+            ss.clear();
+            ss << alignedFilePath << "/t_mark_" << validAlignedId << ".jpg";
+            std::string markedAlignedImgName;
+            ss >> markedAlignedImgName;
             std::ofstream markFout(alignedMarkName);
             markFout << markCount << std::endl;
             for (int markId = 0; markId < markCount; markId++)
             {
                 double xRes, yRes;
                 homoMat.TransformPoint(cvCurMarks.at(markId).x, cvCurMarks.at(markId).y, xRes, yRes);
-                cvSumMarks.at(markId).x += xRes;
-                cvSumMarks.at(markId).y += yRes;
                 markFout << xRes << " " << yRes << std::endl;
+                alignedImg.ptr(int(yRes), int(xRes))[0] = 255;
+                alignedImg.ptr(int(yRes), int(xRes))[1] = 255;
+                alignedImg.ptr(int(yRes), int(xRes))[2] = 255;
             }
             markFout.close();
             
+            cv::imwrite(markedAlignedImgName, alignedImg);
+            alignedImg.release();
             validAlignedId++;
         }
+        fileListOut.close();
         DebugLog << "Finished" << std::endl;
         
         return MAGIC_NO_ERROR;
